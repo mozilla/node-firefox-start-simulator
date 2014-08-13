44c4e0befac94d52082313dbb86444c8017efb1d
nicolagreco <notsecurity@gmail.com>
2014-08-12 18:33:56 -0700

    adding working examples

---
+++ examples/just_simulator.js
@@ -0,0 +1,16 @@
+var startB2G = require('../index');
+var FirefoxClient = require('firefox-client');
+
+startB2G({port:7653, connect:false}, function(err, sim) {
+  var client = new FirefoxClient();
+
+  // Let's show for example all the running apps
+  client.connect(7653, function() {
+    client.getWebapps(function(err, webapps) {
+      webapps.listRunningApps(function(err, apps) {
+        console.log("Running apps:", apps);
+      });
+    });
+  });
+
+});
---
+++ examples/start_and_list_tabs.js
@@ -0,0 +1,12 @@
+var startB2G = require('../index');
+
+startB2G({port:7653}, function(err, client) {
+  // Let's show for example all the running apps
+
+  client.getWebapps(function(err, webapps) {
+    webapps.listRunningApps(function(err, apps) {
+      console.log("Running apps:", apps);
+    });
+  });
+
+});
