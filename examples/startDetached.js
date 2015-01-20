35b4fab37c1fd3349ff9b964b73e4c6c22abb7fe
sole <sole@mozilla.com>
2015-01-20 13:01:25 +0000

    Add start detached example

---
+++ examples/startDetached.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var startSimulator = require('../');
+
+// Start any simulator, in detached and verbose mode
+startSimulator({
+  detached: true
+}).then(function(results) {
+  console.log('Started simulator in detached mode, port = ' + results.port);
+  console.log(results);
+}, function(err) {
+  console.error('Error starting simulator', err);
+});
+
